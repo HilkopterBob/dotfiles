1d3d7264e8bdcdd09c6ebcda4c5013b281ea5ad0
Nick von Podewils <n.podewils@schwerin-sicherheit.de>
2024-08-13 06:27:29 +0200

    feat: disable trackboint with builtin keyboard. Add Hyprland Var for convenience

--- .config/hypr/scripts/SwitchLaptopKeyboard.sh
+++ .config/hypr/scripts/SwitchLaptopKeyboard.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
-HYPRLAND_DEVICE="at-translated-set-2-keyboard"
-HYPRLAND_VARIABLE="device[$HYPRLAND_DEVICE]:enabled"
+HYPRLAND_KB="at-translated-set-2-keyboard"
+HYPRLAND_KB_VARIABLE="device[$HYPRLAND_KB]:enabled"
+
+HYPRLAND_TP="2-elantech-trackpoint"
+HYPRLAND_TP_VARIABLE="device[$HYPRLAND_TP]:enabled"
+
 
 if [ -z "$XDG_RUNTIME_DIR" ]; then
   export XDG_RUNTIME_DIR=/run/user/$(id -u)
@@ -12,14 +16,20 @@ export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
 enable_touchpad() {
   printf "true" >"$STATUS_FILE"
   notify-send -u normal "Enabling Laptop Keyboard"
-  hyprctl keyword $HYPRLAND_VARIABLE true -r
+  hyprctl keyword $HYPRLAND_KB_VARIABLE true -r
+  hyprctl keyword $HYPRLAND_TP_VARIABLE true -r
 }
 
 disable_touchpad() {
   printf "false" >"$STATUS_FILE"
   notify-send -u normal "Disabling Laptop Keyboard"
-  hyprctl keyword $HYPRLAND_VARIABLE false -r
+  hyprctl keyword $HYPRLAND_KB_VARIABLE false -r
+  hyprctl keyword $HYPRLAND_TP_VARIABLE false -r
 }
+
+
+
+
 
 if ! [ -f "$STATUS_FILE" ]; then
   enable_touchpad
@@ -30,3 +40,7 @@ else
     enable_touchpad
   fi
 fi
+
+
+
+
