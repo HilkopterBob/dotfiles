770453b5878f61ddda351426648150056f37a291
Nick <npodewils@schwerin-sicherheit.de>
2025-07-31 09:35:58 +0200

    fix: minor sound bugs

--- .config/hypr/scripts/Sounds.sh
+++ .config/hypr/scripts/Sounds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # This script is used to play system sounds.
-# Script is used by Volume.Sh and ScreenShots.sh 
+# Script is used by Volume.Sh and ScreenShots.sh
 
 theme="freedesktop" # Set the theme for the system sounds.
 mute=false          # Set to true to mute the system sounds.
@@ -12,30 +12,30 @@ muteVolume=false
 
 # Exit if the system sounds are muted.
 if [[ "$mute" = true ]]; then
-    exit 0
+  exit 0
 fi
 
 # Choose the sound to play.
 if [[ "$1" == "--screenshot" ]]; then
-    if [[ "$muteScreenshots" = true ]]; then
-        exit 0
-    fi
-    soundoption="screen-capture.*"
-elif [[ "$1" == "--volume" ]]; then
-    if [[ "$muteVolume" = true ]]; then
-        exit 0
-    fi
-    soundoption="audio-volume-change.*"
-else
-    echo -e "Available sounds: --screenshot, --volume"
+  if [[ "$muteScreenshots" = true ]]; then
     exit 0
+  fi
+  soundoption="screen-capture.*"
+elif [[ "$1" == "--volume" ]]; then
+  if [[ "$muteVolume" = true ]]; then
+    exit 0
+  fi
+  soundoption="audio-volume-change.*"
+else
+  echo -e "Available sounds: --screenshot, --volume"
+  exit 0
 fi
 
 # Set the directory defaults for system sounds.
 if [ -d "/run/current-system/sw/share/sounds" ]; then
-    systemDIR="/run/current-system/sw/share/sounds" # NixOS
+  systemDIR="/run/current-system/sw/share/sounds" # NixOS
 else
-    systemDIR="/usr/share/sounds"
+  systemDIR="/usr/share/sounds"
 fi
 userDIR="$HOME/.local/share/sounds"
 defaultTheme="freedesktop"
@@ -43,9 +43,9 @@ defaultTheme="freedesktop"
 # Prefer the user's theme, but use the system's if it doesn't exist.
 sDIR="$systemDIR/$defaultTheme"
 if [ -d "$userDIR/$theme" ]; then
-    sDIR="$userDIR/$theme"
+  sDIR="$userDIR/$theme"
 elif [ -d "$systemDIR/$theme" ]; then
-    sDIR="$systemDIR/$theme"
+  sDIR="$systemDIR/$theme"
 fi
 
 # Get the theme that it inherits.
@@ -55,18 +55,19 @@ iDIR="$sDIR/../$iTheme"
 # Find the sound file and play it.
 sound_file=$(find $sDIR/stereo -name "$soundoption" -print -quit)
 if ! test -f "$sound_file"; then
-    sound_file=$(find $iDIR/stereo -name "$soundoption" -print -quit)
+  sound_file=$(find $iDIR/stereo -name "$soundoption" -print -quit)
+  if ! test -f "$sound_file"; then
+    sound_file=$(find $userDIR/$defaultTheme/stereo -name "$soundoption" -print -quit)
     if ! test -f "$sound_file"; then
-        sound_file=$(find $userDIR/$defaultTheme/stereo -name "$soundoption" -print -quit)
-        if ! test -f "$sound_file"; then
-            sound_file=$(find $systemDIR/$defaultTheme/stereo -name "$soundoption" -print -quit)
-            if ! test -f "$sound_file"; then
-                echo "Error: Sound file not found."
-                exit 1
-            fi
-        fi
+      sound_file=$(find $systemDIR/$defaultTheme/stereo -name "$soundoption" -print -quit)
+      if ! test -f "$sound_file"; then
+        echo "Error: Sound file not found."
+        exit 1
+      fi
     fi
+  fi
 fi
 
 # pipewire priority, fallback pulseaudio
 pw-play "$sound_file" || pa-play "$sound_file"
+
